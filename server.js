4bec6cb89655bdfc2c89231ec77ef39ab284abfb
daveryde <d.mcordova87@gmail.com>
2019-07-17 16:31:19 -0700

    heroku prebuild

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json({ extended: false }));
 app.use('/search', require('./routes/api/search'));
 app.use('/images', require('./routes/api/images'));
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
