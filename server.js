95a933780145b6991a98850d377e714c3ecbb9c4
daveryde <d.mcordova87@gmail.com>
2019-07-17 16:59:06 -0700

    heroku post setup fix

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
