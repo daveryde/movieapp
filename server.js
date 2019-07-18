2ee9b5b381f02660d07dde12906e84f0d4ab1a78
daveryde <d.mcordova87@gmail.com>
2019-07-17 21:36:01 -0700

    serve static assets

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use('/images', require('./routes/api/images'));
 
 // Serve static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
