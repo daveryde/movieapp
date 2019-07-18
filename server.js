f945de6ea8303e5bc154d0576858636034988087
daveryde <d.mcordova87@gmail.com>
2019-07-17 21:02:34 -0700

    server to client fix

--- server.js
+++ server.js
@@ -3,23 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json({ extended: false }));
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(express.json());
 
 app.use('/search', require('./routes/api/search'));
 app.use('/images', require('./routes/api/images'));
 
 // Serve static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/', express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
