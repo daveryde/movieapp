d3eda333d04e64813b31bb35eef4a4dd7abbce71
daveryde <d.mcordova87@gmail.com>
2019-07-19 09:46:29 -0700

    add urlencoded

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', express.static('client/build'));
 
