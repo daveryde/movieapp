0713ca79be74c35f98e06f8a70567eb40820f06b
d.mcordova87@gmail.com <d.mcordova87@gmail.com>
2020-07-08 22:46:24 -0700

    restructure redux to doc standards

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
+
+// React router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/landing/Landing';
 import SearchView from './components/sections/SearchView';
 import MovieDetail from './components/sections/MovieDetail';
-import { Provider } from 'react-redux';
-import store from './store';
+
+// SCSS compiled CSS
 import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className='container-fluid'>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/searchView' component={SearchView} />
-            <Route exact path='/movieDetail' component={MovieDetail} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <Router>
+      <div className='container-fluid'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'><Landing /></Route>
+          <Route exact path='/searchView'><SearchView /></Route>
+          <Route exact path='/movieDetail'><MovieDetail /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// App component
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, rootElement);
